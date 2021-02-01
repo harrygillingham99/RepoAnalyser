5c453c4b9a9d7314a739bf9e77296933b8b46f5b
harrygillingham99 <harrygillingham99@gmail.com>
2021-02-01 12:23:06 +0000

    feat(config): add CRACO and config file to set up webpack path aliases

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+//CRACO (Create React App Config Override)
+//Configure things here without having to eject the CRA config
+
+const path = require(`path`);
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@state": path.resolve(__dirname, "src/state"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+};
