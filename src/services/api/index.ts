c34584a34331d406b2ec7bec2e4ca7cdb92d137c
harrygillingham99 <harrygillingham99@gmail.com>
2021-02-03 18:01:20 +0000

    feat(client-data) add methods and expand base client to take metadata

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,21 @@
 import { Client, IConfig } from "@services/api/Client";
+import { buildUserInfo } from "@utils/userInfo";
 
-export const authorisedApiClient = (token: string | undefined) =>  new Client(new IConfig(token ?? "not-logged-in"))
+const buildClient = (token: string | (() => string) | undefined) => {
+  const metadata = buildUserInfo();
+  switch (typeof token) {
+    case "function":
+      return new Client(new IConfig(token(), metadata));
+    case "string":
+      return new Client(new IConfig(token, metadata));
+    default:
+      return new Client(new IConfig("not-logged-in", metadata));
+  }
+};
 
-export const apiClient = authorisedApiClient(undefined);
+const apiClient = buildClient(undefined);
+
+const authorisedApiClient = (token: string | (() => string)) =>
+  buildClient(token);
+
+export { apiClient, authorisedApiClient };
---
+++ src/utils/userInfo.ts
@@ -0,0 +1,35 @@
+import { ClientMetadata } from "@services/api/Client";
+
+function pageon() {
+  return window.location.pathname;
+}
+
+function referrer() {
+  return document.referrer;
+}
+
+function browserName() {
+  return navigator.appName;
+}
+
+function browserEngine() {
+  return navigator.product;
+}
+
+function browserLanguage() {
+  return navigator.language;
+}
+
+function dataCookiesEnabled() {
+  return navigator.cookieEnabled;
+}
+
+export const buildUserInfo = () =>
+  new ClientMetadata({
+    page: pageon(),
+    referrer: referrer(),
+    browserName: browserName(),
+    browserEngine: browserEngine(),
+    browserLanguage: browserLanguage(),
+    cookiesEnabled: dataCookiesEnabled(),
+  });
