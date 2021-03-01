7b55efee4e36265cfc00986051e6f296c61126a3
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-01 18:32:16 +0000

    fix(typing): fix the types in the conditional wrapper component to infer the children

--- src/components/BaseComponents/ConditionalWrapper.tsx
+++ src/components/BaseComponents/ConditionalWrapper.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 
 interface IConditionalWrapperProps {
-  children: React.ReactElement;
   condition: boolean;
-  wrapper: (children: React.ReactElement) => JSX.Element;
+  wrapper: (children: React.ReactNode) => JSX.Element;
 }
 
-export const ConditonalWrapper: React.FC<IConditionalWrapperProps> = (props) =>
-  props.condition ? props.wrapper(props.children) : props.children;
+export const ConditonalWrapper: React.FC<IConditionalWrapperProps> = (
+  props: PropsWithChildren<IConditionalWrapperProps>
+): JSX.Element =>
+  props.condition ? props.wrapper(props.children) : <>{props.children}</>;
