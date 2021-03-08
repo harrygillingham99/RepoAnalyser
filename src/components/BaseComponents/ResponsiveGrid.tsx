361435341b86529f37005bc8e2d2ff6925be0db1
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-08 13:38:45 +0000

    feat(grid): ResponsiveGrid will take children or items with a mapper func

--- src/components/BaseComponents/ResponsiveGrid.tsx
+++ src/components/BaseComponents/ResponsiveGrid.tsx
@@ -1,11 +1,15 @@
 import Masonry from "react-masonry-css";
 import "@styles/Masonry.scss";
+import { PropsWithChildren } from "react";
 
 interface IResponsiveGridProps<T> {
-  items: T[];
-  mapFunc: (item: T) => JSX.Element;
+  items?: T[];
+  mapFunc?: (item: T) => JSX.Element;
 }
-export function ResponsiveGrid<T>(props: IResponsiveGridProps<T>) {
+/* Will accept children for the grid OR a list of items with a mapper function  */
+export function ResponsiveGrid<T>(
+  props: PropsWithChildren<IResponsiveGridProps<T>>
+) {
   return (
     <Masonry
       breakpointCols={{
@@ -18,7 +22,7 @@ export function ResponsiveGrid<T>(props: IResponsiveGridProps<T>) {
       className="masonry-grid mt-1"
       columnClassName="masonry-grid_column"
     >
-      {props.items.map((item) => props.mapFunc(item))}
+      {props.children ?? props!.items!.map((item) => props!.mapFunc!(item))}
     </Masonry>
   );
 }
