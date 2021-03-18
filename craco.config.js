205cc97f1ec6e0e3ccbdde9b24b789ff17762a58
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-18 19:48:56 +0000

    feat(webpack): add stylelint and use https devserver

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,33 @@
 const CracoAlias = require("craco-alias");
+const { whenDev } = require("@craco/craco");
+const path = require("path")
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
-   plugins: [
-     {
-        plugin: CracoAlias,
-        options: {
-           source: "tsconfig",
-           // baseUrl SHOULD be specified
-           // plugin does not take it from tsconfig
-           baseUrl: "./src",
-           /* tsConfigPath should point to the file where "baseUrl" and "paths" 
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        // baseUrl SHOULD be specified
+        // plugin does not take it from tsconfig
+        baseUrl: "./src",
+        /* tsConfigPath should point to the file where "baseUrl" and "paths" 
            are specified*/
-           tsConfigPath: "./tsconfig.paths.json"
-        }
-     }
-  ]
+        tsConfigPath: "./tsconfig.paths.json",
+      },
+    },
+  ],
+  webpack: {
+    devServer: whenDev(() => ({ https: true })),
+    plugins: {
+      add: [
+        new StyleLintPlugin({
+          configBasedir: __dirname,
+          context: path.resolve(__dirname, "src"),
+          files: ["**/*.scss"],
+        }),
+      ],
+    },
+  },
 };
