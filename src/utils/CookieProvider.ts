9f75e28fae04d8885398dbc9a981961fa11ba0e0
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-19 00:43:40 +0000

    fix: tidy up

--- src/utils/CookieProvider.ts
+++ src/utils/CookieProvider.ts
@@ -1,40 +1,35 @@
-export function setCookie<T> (name: string, val: T) {
+export function setCookie<T>(name: string, val: T) {
   const date = new Date();
-  const value = mapToCookieValue(val)
-
+  const value = mapToCookieValue<T>(val);
   // Set it expire in 7 days
   date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
-
   // Set it
   document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/; Secure; SameSite=Strict`;
-};
+}
 
-export function getCookie<T>(name: string) : T | undefined  {
+export function getCookie<T>(name: string): T | undefined {
   const value = "; " + document.cookie;
   const parts = value.split("; " + name + "=");
-
   if (parts && parts.length === 2) {
-    return mapFromCookieValue(parts.pop()?.split(";").shift());
+    return mapFromCookieValue<T>(parts.pop()?.split(";").shift());
   }
-};
+}
 
 export const expireCookie = (name: string) => {
   const date = new Date();
-
   // Set it expire in -1 days
   date.setTime(date.getTime() + -1 * 24 * 60 * 60 * 1000);
-
   // Set it
   document.cookie = `${name}=; expires=${date.toUTCString()}; path=/; Secure; SameSite=Strict`;
 };
 
-function mapFromCookieValue<T>(stringVal? : string) : T | undefined {
-  if(!stringVal || stringVal.length === 0) return;
-  return JSON.parse(stringVal) as T
+function mapFromCookieValue<T>(stringVal?: string): T | undefined {
+  if (!stringVal || stringVal.length === 0) return;
+  return JSON.parse(stringVal) as T;
 }
 
-function mapToCookieValue<T> (value : T) :  string {
-  switch(typeof(value)){
+function mapToCookieValue<T>(value: T): string {
+  switch (typeof value) {
     case "object":
     case "string":
     case "number":
@@ -42,6 +37,6 @@ function mapToCookieValue<T> (value : T) :  string {
     case "function":
       return mapToCookieValue(value());
     default:
-      throw new Error(`Cannot map ${typeof(value)} to cookie value`)
+      throw new Error(`Cannot map ${typeof value} to cookie value`);
   }
 }
