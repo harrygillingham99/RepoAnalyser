ea5b66088eff6f04b2ab503c08c9a9ad0395b4bf
harrygillingham99 <harrygillingham99@gmail.com>
2021-04-08 13:48:45 +0100

    feat(Activity): added vertical timeline component todo pagination

--- src/components/Routes/Home/Activity/ActivityRoute.tsx
+++ src/components/Routes/Home/Activity/ActivityRoute.tsx
@@ -5,8 +5,25 @@ import { authorisedApiClient } from "@services/api/Index";
 import { AlertContainer } from "@state/AlertContainer";
 import { AppContainer } from "@state/AppStateContainer";
 import { buildUserInfo } from "@utils/ClientInfo";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useEffectOnce, useSetState } from "react-use";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import {
+  Github,
+  ArrowUp,
+  Arrow90degRight,
+  ArrowBarRight,
+  Search,
+  CloudPlus,
+  People,
+  Asterisk,
+  PersonPlus,
+  X,
+} from "react-bootstrap-icons";
 
 interface IActivityRouteState {
   userActivity: UserActivity;
@@ -34,10 +51,75 @@ export const ActivityRoute = () => {
       }
     })();
   });
+
+  const getIconColourFriendlyNameForEvent = (
+    eventType?: string
+  ): [string, string, React.ReactNode] => {
+    switch (eventType) {
+      case "PullRequestEvent":
+        return ["Pull Request Event", "#28a745", <Arrow90degRight />];
+      case "PushEvent":
+        return ["Push", "#17a2b8", <ArrowUp />];
+      case "ForkEvent":
+        return ["Fork", "#17a2b8", <ArrowBarRight />];
+      case "PullRequestReviewEvent":
+        return ["Pull Request Review", "#28a745", <Search />];
+      case "ReleaseEvent":
+        return ["Release", "blue", <Asterisk />];
+      case "PublicEvent":
+        return ["Public Event", "#ffc107", <People />];
+      case "MemberEvent":
+        return ["Collaborator Event", "#5a6268", <PersonPlus />];
+      case "CreateEvent":
+        return ["Create", "#5a6268", <CloudPlus />];
+      case "DeleteEvent":
+        return ["Delete", "#dc3545", <X />];
+      default:
+        return [eventType ?? "Unknown", "blue", <Github />];
+    }
+  };
+
   return (
     <>
-      <DashboardHeader text="Activity" />{" "}
-      {!loading && JSON.stringify(state.userActivity)}
+      <DashboardHeader text="Activity" />
+      {!loading && state.userActivity && state.userActivity.events && (
+        <VerticalTimeline>
+          {state.userActivity.events.map((event) => {
+            const [
+              friendlyEventName,
+              colour,
+              icon,
+            ] = getIconColourFriendlyNameForEvent(event.type);
+            return (
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  background: colour,
+                  color: "#fff",
+                }}
+                contentArrowStyle={{
+                  borderRight: `7px solid  ${colour}`,
+                }}
+                date={event.createdAt?.toDateString()}
+                iconStyle={{ background: colour, color: "#fff" }}
+                icon={icon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {friendlyEventName}
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  {event.repo?.name}
+                </h4>
+                <p>
+                  {`Created at: ${event.createdAt?.toLocaleString("en-GB", {
+                    timeZone: "UTC",
+                  })}`}
+                </p>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+      )}
       {loading && <Loader />}
     </>
   );
