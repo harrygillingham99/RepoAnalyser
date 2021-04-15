14bb242543ca79b54626ce9833f0b9e86404712b
harrygillingham99 <harrygillingham99@gmail.com>
2021-04-15 17:14:17 +0100

    feat(detailed-repo): added commit timeline view for each file

--- src/components/Routes/Home/Repositories/DetailedRepositoryRoute.tsx
+++ src/components/Routes/Home/Repositories/DetailedRepositoryRoute.tsx
@@ -1,16 +1,21 @@
 import { DashboardHeader } from "@components/BaseComponents/DashboardHeader";
 import { Loader } from "@components/BaseComponents/Loader";
 import { DirectoryTree } from "@components/BaseComponents/DirectoryTree";
-import { DetailedRepository } from "@services/api/Client";
+import { DetailedRepository, GitHubCommit } from "@services/api/Client";
 import { authorisedApiClient } from "@services/api/Index";
 import { AlertContainer } from "@state/AlertContainer";
 import { AppContainer } from "@state/AppStateContainer";
 import { Routes } from "@typeDefinitions/Routes";
 import { buildUserInfo } from "@utils/ClientInfo";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import { Redirect, useParams } from "react-router-dom";
 import { useEffectOnce, useSetState } from "react-use";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { Github } from "react-bootstrap-icons";
 
 interface RouteParams {
   repoId: string;
@@ -21,10 +26,12 @@ interface DetailedRepositoryRouteState {
   repo: DetailedRepository;
   activeTab: string;
   selectedFile?: string;
+  fileCommits?: GitHubCommit[];
 }
 
 export const DetailedRepositoryRoute = () => {
   const [loading, setLoading] = React.useState<boolean>(false);
+  const [loadingFileInfo, setLoadingFileInfo] = React.useState<boolean>(false);
   const { repoId, repoName } = useParams<RouteParams>();
   const repoNumber = Number.parseInt(repoId);
   const { appState } = AppContainer.useContainer();
@@ -53,6 +60,36 @@ export const DetailedRepositoryRoute = () => {
       }
     })();
   });
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (
+          state.selectedFile &&
+          state.repo?.repository?.id !== undefined &&
+          state.selectedFile.indexOf(".") >= 0
+        ) {
+          console.log(state.selectedFile);
+          setLoadingFileInfo(true);
+          const splitFileName = state.selectedFile.split(".");
+          const result = await authorisedApiClient(
+            appState.token
+          ).repository_GetFileInformation(
+            state.repo.repository.id,
+            splitFileName[0],
+            splitFileName[1],
+            buildUserInfo
+          );
+          setState({ fileCommits: result });
+        }
+      } catch (error) {
+        showErrorAlert("Error", "Error getting commits for file");
+      } finally {
+        setLoadingFileInfo(false);
+        console.log(state.fileCommits);
+      }
+    })();
+  }, [state.selectedFile]);
 
   const recalculateCodeOwners = async () => {
     if (!state.repo || !state.repo.repository || !state.repo.repository.id)
@@ -93,7 +130,7 @@ export const DetailedRepositoryRoute = () => {
         <Tab eventKey="Code Owners" title="Code Owners">
           <Container className="mt-1" fluid>
             <Row>
-              <Col>
+              <Col sm={4}>
                 <Button
                   className="mb-2"
                   variant="info"
@@ -124,21 +161,64 @@ export const DetailedRepositoryRoute = () => {
               {state.repo?.codeOwners &&
                 Object.keys(state.repo.codeOwners).length > 1 &&
                 !loading && (
-                  <Col>
-                    <p>File: {selectedFile}</p>
-                    <p>
-                      Owned By:{" "}
-                      {state.repo?.codeOwners !== undefined && selectedFile
-                        ? state.repo.codeOwners[selectedFile]
-                        : "Unknown"}
-                    </p>
+                  <Col sm={8}>
+                    <Row></Row>
+                    <Col>
+                      <h6 className="d-flex">File: {selectedFile}</h6>
+                    </Col>
+                    <Col>
+                      <h6 className="d-flex">
+                        Owned By:{" "}
+                        {state.repo?.codeOwners !== undefined && selectedFile
+                          ? state.repo.codeOwners[selectedFile]
+                          : "Unknown"}
+                      </h6>
+                    </Col>
+                    {state.fileCommits &&
+                      state.selectedFile &&
+                      !loadingFileInfo && (
+                        <VerticalTimeline>
+                          {state.fileCommits.map((commit) => {
+                            const colour = "#17a2b8";
+                            return (
+                              <VerticalTimelineElement
+                                key={`timeLineItem-${commit.sha}`}
+                                className="vertical-timeline-element--work"
+                                contentStyle={{
+                                  background: colour,
+                                  color: "#fff",
+                                }}
+                                contentArrowStyle={{
+                                  borderRight: `7px solid  ${colour}`,
+                                }}
+                                iconStyle={{
+                                  background: colour,
+                                  color: "#fff",
+                                }}
+                                icon={<Github />}
+                              >
+                                <h4 className="vertical-timeline-element-title">
+                                  {commit.author?.login}
+                                </h4>
+                                <h5 className="vertical-timeline-element-subtitle">
+                                  <a
+                                    href={commit.htmlUrl}
+                                    className="text-reset"
+                                  >
+                                    {commit.commit?.message}
+                                  </a>
+                                </h5>
+                                <p></p>
+                              </VerticalTimelineElement>
+                            );
+                          })}
+                        </VerticalTimeline>
+                      )}
+                    {loadingFileInfo && <Loader />}
                   </Col>
                 )}
             </Row>
           </Container>
-        </Tab>
-        <Tab eventKey="Commits" title="Commits">
-          {state.repo && !loading && <>{JSON.stringify(state.repo)}</>}
         </Tab>
         <Tab eventKey="Complexity Analysis" title="Complexity Analysis"></Tab>
       </Tabs>
